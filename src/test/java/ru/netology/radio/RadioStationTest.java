27a4c098764105b240a4ebcd9c48777079b162e4
“Vladimir_Grin” <mortywallet@gmail.com>
2023-10-12 21:25:36 +0300

    QAradio2

--- src/test/java/ru/netology/radio/RadioStationTest.java
+++ src/test/java/ru/netology/radio/RadioStationTest.java
@@ -7,58 +7,7 @@ public class RadioStationTest {
 
     @Test
     public void test() {
-        Radio radio = new Radio();
-        radio.setRadioStation(7);
-
-        int expected = 7;
-        int actual = radio.getRadioStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test2() {
-        Radio radio = new Radio();
-        radio.setRadioStation(219);
-
-        int expected = 0;
-        int actual = radio.getRadioStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test3() {
-        Radio radio = new Radio();
-        radio.setRadioStation(-1);
-
-        int expected = 0;
-        int actual = radio.getRadioStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test4() {
-        Radio radio = new Radio();
-        radio.setRadioStation(4);
-        radio.setRadioStation(0);
-
-        int expected = 0;
-        int actual = radio.getRadioStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test5() {
-        Radio radio = new Radio();
-        radio.setRadioStation(1);
-
-        int expected = 1;
-        int actual = radio.getRadioStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test6() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10);
         radio.setRadioStation(8);
 
         int expected = 8;
@@ -67,10 +16,9 @@ public class RadioStationTest {
     }
 
     @Test
-    public void test7() {
-        Radio radio = new Radio();
+    public void test2() {
+        Radio radio = new Radio(10);
         radio.setRadioStation(9);
-
 
         int expected = 9;
         int actual = radio.getRadioStation();
@@ -78,13 +26,52 @@ public class RadioStationTest {
     }
 
     @Test
-    public void test8() {
-        Radio radio = new Radio();
+    public void test3() {
+        Radio radio = new Radio(10);
         radio.setRadioStation(10);
-
 
         int expected = 0;
         int actual = radio.getRadioStation();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void test4() {
+        Radio radio = new Radio(10);
+        radio.setRadioStation(11);
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test5() {
+        Radio radio = new Radio(10);
+        radio.setRadioStation(0);
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() {
+        Radio radio = new Radio(10);
+        radio.setRadioStation(1);
+
+        int expected = 1;
+        int actual = radio.getRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void test7() {
+        Radio radio = new Radio(10);
+        radio.setRadioStation(-1);
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
